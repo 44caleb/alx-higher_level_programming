b2a5943aad85e7f407504d176a6a9c7f355ab136
44caleb <calebadinfono@gmail.com>
2023-06-08 13:09:57 -0700

    adas

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3 
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    plus = add(a, b)
+    minus = sub(a, b)
+    prod = mul(a, b)
+    divide = div(a, b)
+
+    print("{} + {} = {}".format(a, b, plus))
+    print("{} - {} = {}".format(a, b, minus))
+    print("{} * {} = {}".format(a, b, prod))
+    print("{} / {} = {}".format(a, b, divide))
