45531aac9bae9ffd7a5ed009a505a235f48f8e5a
44caleb <calebadinfono2@gmail.com>
2023-09-28 15:10:30 -0700

    push 3

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a delete request method to a url
-curl -sX DELETE $1i
+curl -sX DELETE $1
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #posts data to a url
-#!/bin/bash
-#takes in a url and sends a POST request and displays response"
 curl -sd "test@gmail.com&subject=I will always be here for PLD" $1
